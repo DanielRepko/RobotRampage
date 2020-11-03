7dc54af25313aefa59a3767c7fe53f6e6ff2579a
Daniel Repko <danielrepko07@gmail.com>
2020-11-03 15:49:13 -0500

    Add animations to guns and create GameUI object and script

---
+++ Assets/Scripts/GameUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameUI : MonoBehaviour
+{
+    // SerializeField makes the properties accessible from the inspector but not other scripts
+    [SerializeField]
+    Sprite redReticle;
+    [SerializeField]
+    Sprite yellowReticle;
+    [SerializeField]
+    Sprite blueReticle;
+    [SerializeField]
+    Image reticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
