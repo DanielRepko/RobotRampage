ed3f08afd796d6e7b00de9c2dafe8f4d3a269f23
Daniel Repko <danielrepko07@gmail.com>
2020-11-03 15:29:54 -0500

    Create gun scripts and animation controllers

---
+++ Assets/Scripts/AssaultRifle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssaultRifle : Gun
+{
+    // Update is called once per frame
+    override protected void Update()
+    {
+        base.Update();
+        //Shotgun & Pistol have semi auto fire rate
+        if (Input.GetMouseButton(0) && (Time.time - lastFireTime > fireRate))
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
---
+++ Assets/Scripts/Pistol.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Gun
+{
+    // Update is called once per frame
+    override protected void Update()
+    {
+        base.Update();
+        //Shotgun & Pistol have semi auto fire rate
+        if (Input.GetMouseButtonDown(0) && (Time.time - lastFireTime > fireRate))
+        {
+            lastFireTime = Time.time;
+            Fire();
+        }
+    }
+}
