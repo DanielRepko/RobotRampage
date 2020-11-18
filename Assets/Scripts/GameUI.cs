8ef9d632477a3a6750a34e28dc724051531671cf
Daniel Repko <danielrepko07@gmail.com>
2020-11-18 11:24:01 -0500

    Add text to GameUI

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -15,6 +15,28 @@ public class GameUI : MonoBehaviour
     [SerializeField]
     Image reticle;
 
+    //fields for all of the ui text
+    [SerializeField]
+    private Text ammoText;
+    [SerializeField]
+    private Text healthText;
+    [SerializeField]
+    private Text armorText;
+    [SerializeField]
+    private Text scoreText;
+    [SerializeField]
+    private Text pickupText;
+    [SerializeField]
+    private Text waveText;
+    [SerializeField]
+    private Text enemyText;
+    [SerializeField]
+    private Text waveClearText;
+    [SerializeField]
+    private Text newWaveText;
+    [SerializeField]
+    Player player;
+
     // changes the reticle being used
     public void UpdateReticle()
     {
