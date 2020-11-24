30d85b0bbbc8f1542d79217946d27791326737de
Daniel Repko <danielrepko07@gmail.com>
2020-11-23 19:26:11 -0500

    Create the main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //loads the battle scene
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+
+    //closes the game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
